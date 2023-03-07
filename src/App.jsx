53f6fd9812e6b0172cea7d65174e5f9d8da2eb35
Edwin Rozo Gómez <erozog125@gmail.com>
2023-03-07 11:29:51 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { ContactUs } from './ContactUs/ContactUs'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Formulario de contáctenos</h1>
+      <ContactUs />
+    </div>
+  )
+}
+
+export default App
---
+++ src/ContactUs/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const ContactUs = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder='Name' />
+        <input type="email" placeholder='email' />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
