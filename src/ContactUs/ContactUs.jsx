2798b54af8f5f28c5fa7d46298a05c3c3faa8dd4
sararussi <russibonillas@gmail.com>
2023-03-07 23:20:03 -0500

    Cambios

--- src/ContactUs/ContactUs.jsx
+++ src/ContactUs/ContactUs.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import './contactus.css'
 
 export const ContactUs = () => {
   return (
     <div>
       <form action="">
+        <div className='header-contact'>
+          <h2>ContactUs</h2>
+          <p>Please fill  this form</p>
+        </div>
+        <fieldset className='field-name'>
         <input type="text" placeholder='Name' />
+        </fieldset >
+        <fieldset className='field-email'>
         <input type="email" placeholder='email' />
-        <button>Submit</button>
+        </fieldset>
+        <fieldset className='field-message'>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </fieldset>
+
+        <button className='btn-send '>Submit</button>
+        
       </form>
     </div>
   )
