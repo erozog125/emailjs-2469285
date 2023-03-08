7a6edba076afb01b0bc9cb54addf9892f2e11662
cristian morales <cm0767407@gmail.com>
2023-03-07 22:40:51 -0600

    subiendo proyecto

--- src/ContactUs/ContactUs.jsx
+++ src/ContactUs/ContactUs.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React,{useRef} from 'react'
+import './contactus.css';
+import emailjs from '@emailjs/browser';
 
 export const ContactUs = () => {
+
+  const refForm = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //  console.log(refForm.current);
+    const ServiceId = "service_aoyjsmq";
+    const templateId = "template_zqgceoo";
+    const apikey = "Q88AmNookpcytsg2d"
+
+    emailjs.sendForm(ServiceId,templateId,refForm.current,apikey)
+    .then (result => console.log(result.text))
+    .catch (error => console.error(error))
+    
+  }
+
+
   return (
-    <div>
-      <form action="">
-        <input type="text" placeholder='Name' />
-        <input type="email" placeholder='email' />
-        <button>Submit</button>
-      </form>
-    </div>
+    <form ref={refForm} action="" onSubmit={handleSubmit}>
+      <div className='header-contact'>
+      <h2>contact Us</h2>
+      <p>please fill this form</p>
+      </div>
+
+      <fieldset className='field-name'>
+      <label className='symbol-required name' htmlFor="">Name</label>
+      <input name= 'username' type="text" placeholder='ejemplo: juan s'  required/>
+      </fieldset>
+
+      <fieldset className='field-email'>
+      <label className='symbol-required ' name="email">Email</label>
+      <input name= 'email' type="email" id ="email" placeholder='ejemplo: ds6714955@gmail.com'  required/>
+      </fieldset>
+
+      <fieldset className='field-message'>
+      <label className='symbol-required '>Email</label>
+      <textarea maxLength={500} placeholder="type you message" name='message' id='' cols={30} rows ={10}  required/>
+      </fieldset>
+     
+      <button className='btn-send'>Send</button>
+
+    </form>
   )
 }
